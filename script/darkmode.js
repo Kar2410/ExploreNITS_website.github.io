0d703aad249f2fc81265ffecd282dc1da9d57965
Arkadeep Kashyap <arkadeep.kashyap39@gmail.com>
2020-11-10 12:17:49 +0530

    darkmode bug fixed

--- script/darkmode.js
+++ script/darkmode.js
@@ -5,3 +5,11 @@
   body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
 }
 console.log("bhhah");
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let toggle = document.querySelector("fieldset.toggle");
+  toggle.classList.toggle("hidden");
+});
+
